87db14ca0b03c1424f9ac5856c4b6ab5298fb39c
Sascha Seniuk <sascha.seniuk@gmail.com>
2021-01-27 11:09:22 +0100

    WORKING

---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script>
+	import successkid from "images/successkid.jpg";
+</script>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<h1 class="text-2xl text-red-500">Great success!</h1>
+
+<figure>
+	<img alt="Success Kid" src={successkid} />
+	<figcaption>Have fun with Sapper!</figcaption>
+</figure>
+
+<p>
+	<strong
+		>Try editing this file (src/routes/index.svelte) to test live reloading.</strong
+	>
+</p>
